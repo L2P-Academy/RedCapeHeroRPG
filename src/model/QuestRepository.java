952d0c4aee1ff515529b58e50c3e14749f6d4048
CC-Student <CC-Student@MININT-CT9FTOP.speedport.ip>
2026-07-24 17:06:46 +0200

    QuestRepository erstellt

--- src/model/QuestRepository.java
+++ src/model/QuestRepository.java
@@ -1,5 +1,19 @@
 package model;
 
-public class QuestRepository {
+import java.util.ArrayList;
+import java.util.List;
 
+public class QuestRepository {
+    // Hier werden die Quests im Speicher gehalten
+    private List<QuestModel> questList = new ArrayList<>();
+
+    // Alle Quests abrufen
+    public List<QuestModel> getAllQuests() {
+        return questList;
+    }
+
+    // Neue Quest hinzufügen
+    public void addQuest(QuestModel quest) {
+        questList.add(quest);
+    }
 }
