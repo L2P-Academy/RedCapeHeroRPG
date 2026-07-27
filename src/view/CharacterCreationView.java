2d5e3766182eea46a74ae0587f67c4d6579c124e
HerrBert <jensbrenke@proton.me>
2026-07-27 10:34:24 +0200

    Name Field added

--- src/view/CharacterCreationView.java
+++ src/view/CharacterCreationView.java
@@ -1,8 +1,13 @@
 package view;
-
 //(Jens)
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import java.util.List;
+import model.ClassModel;
+import repository.ClassRepository;
 
 public class CharacterCreationView extends JDialog {
 	
@@ -10,8 +15,13 @@ public class CharacterCreationView extends JDialog {
 		setTitle("Charakter Erstellung"); // Titel
 		setSize(600, 500); //Menügröße
 		setModal(true); // Verhindert das ein anderes Fenster geöffnet wird
-		setLocationRelativeTo(null);
-		setVisible(true);
+		setLocationRelativeTo(null); //zentriert das Fenster relativ zum Hauptfenster
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE); //Damit das Fenster auch wieder zugeht
+		
+		JLabel nameLabel = new JLabel ("Charakter Name:");
+		JTextField nameField = new JTextField(20);
+		add (nameLabel);
+		add (nameField);
+		List <ClassModel> classList = ClassRepository.getClasslist();
 	}
-
 }
