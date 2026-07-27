aab2cc23b4943630e6c3c1b0e2d9ca1a3af5af82
CC-Student <CC-Student@MININT-CT9FTOP>
2026-07-27 16:06:45 +0200

    QuestRepository 10 Quests zur Questlist hinzugefuegt

--- src/model/QuestRepository.java
+++ src/model/QuestRepository.java
@@ -8,9 +8,16 @@ public class QuestRepository {
     private static final List<QuestModel> QUEST_LIST = new ArrayList<>();
 
     static {
-        QUEST_LIST.add(new QuestModel(0, "Startquest", "Der Schmied hat ein Schwert für dich angefertigt!", false));
-        QUEST_LIST.add(new QuestModel(1, "Nächste Quest...", "", false));
-        QUEST_LIST.add(new QuestModel(2, "Übernächste Quest...", "", false));
+        QUEST_LIST.add(new QuestModel(0, "Das Erwachen", "Der Schmied hat ein Schwert aus den Trümmern gerettet. Hol es dir!", false));
+        QUEST_LIST.add(new QuestModel(1, "Erste Wölfe", "Töte 5 Schattenwölfe im dunklen Wald, um den Weg freizumachen.", false));
+        QUEST_LIST.add(new QuestModel(2, "Die verlassene Mine", "Finde den alten Mineneingang und beschaffe 3 Leuchtkristalle.", false));
+        QUEST_LIST.add(new QuestModel(3, "Der verfluchte Trank", "Bringe dem Alchemisten die Zutaten für ein Gegengift.", false));
+        QUEST_LIST.add(new QuestModel(4, "Wache der Finsternis", "Besiege den Torwächter der alten Festung.", false));
+        QUEST_LIST.add(new QuestModel(5, "Das verlorene Buch", "Suche in den Archiven nach den Glyphen des Schattenfürsten.", false));
+        QUEST_LIST.add(new QuestModel(6, "Schmiedefeuer", "Schmiede deine Klinge neu mit dem magischen Erz.", false));
+        QUEST_LIST.add(new QuestModel(7, "Hinterhalt im Sumpf", "Überlebe den Angriff der Schattenkultisten.", false));
+        QUEST_LIST.add(new QuestModel(8, "Das Portalsiegel", "Zerstöre die 3 Dämonensteine, um das Tor zu schwächen.", false));
+        QUEST_LIST.add(new QuestModel(9, "Der Schattenfürst", "Betritt den Thronsaal und vernichte den Schattenfürsten!", false));
     }
 
     public static List<QuestModel> getQuestList() {
