c4a6fefb73e56446598a4aced8d35c5f3c5d0630
Denis Gotz | FISI Umschulung <gotzdenis@gmail.com>
2026-07-27 10:35:35 +0200

    open new NPC Models

--- src/model/NPCModel.java
+++ src/model/NPCModel.java
@@ -1,5 +1,69 @@
 package model;
 
+//Denis
 public class NPCModel {
 
+	// attributes
+	private int id;
+	private String name;
+	private String dialog;
+	private int positionX;
+	private int positionY;
+	
+	// constructor
+	public NPCModel(int id, String name, String dialog, int positionX, int positionY, String spritePath) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dialog = dialog;
+		this.positionX = positionX;
+		this.positionY = positionY;
+	}
+
+	public NPCModel(int id2, String name2, String dialog2, int positionX2, int positionY2, int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	//getter & setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDialog() {
+		return dialog;
+	}
+
+	public void setDialog(String dialog) {
+		this.dialog = dialog;
+	}
+
+	public int getPositionX() {
+		return positionX;
+	}
+
+	public void setPositionX(int positionX) {
+		this.positionX = positionX;
+	}
+
+	public int getPositionY() {
+		return positionY;
+	}
+
+	public void setPositionY(int positionY) {
+		this.positionY = positionY;
+	}
+
+	
 }
--- src/model/NPCRepository.java
+++ src/model/NPCRepository.java
@@ -1,5 +1,20 @@
 package model;
+//Denis
+
+import java.util.*;
 
 public class NPCRepository {
+	private static final List<NPCModel> NPCLIST = new ArrayList<NPCModel>();
+	
+	static {
+		NPCLIST.add(new NPCModel(0, "Wache1Light", "WacheLight", 2, 2, 3));
+		NPCLIST.add(new NPCModel(1, "Wache2Middle", "WacheMiddle", 3, 2, 4));
+		NPCLIST.add(new NPCModel(2, "Wache3Hard", "WacheHard", 4, 2, 6));
+		NPCLIST.add(new NPCModel(3, "Wache4Ultra", "WacheUltra", 6, 5, 6));
+	}
 
+	public static List<NPCModel> getNpclist() {
+		return NPCLIST;
+	}
+	
 }
